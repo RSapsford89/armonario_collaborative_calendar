5af183b1afb45c39f8a171baf0cbdae63789321a
RSapsford89 <treborwar@live.co.uk>
2025-10-15 14:38:27 +0100

    started design of model for group profile table.

--- group_profile/models.py
+++ group_profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#needs @isAuth signal on the view so that the user profile info can be used to populate some fields
+class GroupProfile():
+    GroupName = models.CharField(blank=False max_length=128)
+    GroupOwner = #This needs to link to the table of the user... Either Junction table or not?
+    GroupColour = models.CharField(blank=False, default='FFFFFF', max_length=6) #hex colour value, should link to color icker hex on the html side
+    GroupShareCode = models.CharField(blank=False, max_length=12)#a random 12 character share code, generated when creating at the form?
+
+    def __str__(self):
+        return self.GroupName
