dfdcf83e48aff55a34900f1bbd528b3f161403a7
RSapsford89 <treborwar@live.co.uk>
2025-10-28 23:22:55 +0000

    migrate model

--- armonario_proj/settings.py
+++ armonario_proj/settings.py
@@ -46,7 +46,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Add this back
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', ]
 
